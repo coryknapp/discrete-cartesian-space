57ffe283e077025d1bf0e340adad61f3c45000d4
Cory Knapp <knapp.cory@gmail.com>
2015-07-09 23:06:29 -0700

    added bounds function

--- Space.hpp
+++ Space.hpp
@@ -3,6 +3,7 @@
 
 #include <map>
 #include <vector>
+#include <numeric>
 namespace dcs{
 
 static size_t chunkSize = 32;
@@ -28,6 +29,26 @@ public:
 		int chunkJ = j/chunkSize;
 		m_map[ std::pair<int,int>( chunkI, chunkJ ) ].
 			m_grid[i%chunkSize][j%chunkSize] = v;
+	}
+
+	void bounds(int &leftInclusive, int &rightExclusive,
+				int &bottomInclusive, int &topExclusive ){
+		leftInclusive = std::numeric_limits<int>::max();
+		rightExclusive = std::numeric_limits<int>::min();
+		bottomInclusive = std::numeric_limits<int>::max();
+		topExclusive = std::numeric_limits<int>::min();
+		for (auto &i : m_map ) {
+			if( i.first.first < leftInclusive )
+				leftInclusive = i.first.first;
+			if( i.first.first > rightExclusive ) 
+				rightExclusive = i.first.first + 1;
+			if( i.first.second < bottomInclusive )
+				bottomInclusive = i.first.second;
+			if( i.first.second > topExclusive) 
+				topExclusive = i.first.second + 1;
+
+		}
+
 	}
 	
 private:
